ab34de88ab53a5b61165964ed0e1dc1072c98373
glflores <glflores@indra.es>
2019-11-28 09:20:01 +0100

    añade entidades receta y notas

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping
+    @RequestMapping({"", "/", "/index"})
     public String getIndex() {
+        System.out.println("Some message to say... 123 55555");
         return "index";
     }
 }
